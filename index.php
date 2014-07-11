231ee42cde2091a06336ff1b7c93ecd6521821c7
moonwon <valentiney0214@gmail.com>
2014-07-11 23:32:46 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,17 @@
   $db_user = "ec2-user";
 
   $conn = mysqli_connect($db_host, $db_user, "", "testa");
-  if (mysqli_connect_errno($conn)) {
-    echo "DB Fail".mysqli_connect_error();
-  }
-  else {
+  if ($conn) {
+    echo "DB Connected";
+    
     $result = mysql_qurry("SELECT * FROM USER;");
     
     while ($row = mysql_fetch_array($result)) {
       echo $row[0];
     }
+  }
+  else {
+    echo "DB Fail";
   }
 
 ?>
