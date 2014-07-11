215ab4a5f6eba9ba62bad846feeabc24510bb7d1
moonwon <valentiney0214@gmail.com>
2014-07-11 23:14:00 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
-<? 
- include "queryFunctions.php"
-?>
+<?
+fuction queryTest($db_name, $query) {
 
-<html>
-<head>
-<title>Hello World!</title>
-</head>
-<body>
-"query : "
-<?php queryTest("test", "SELECT * FROM USER;") ?>
-</body>
-</html>
+  $db_host = "localhost";
+  $db_user = "root";
+
+  $conn = mysqli_connect($db_host, $db_user, "", $db_name);
+  if (mysqli_connect_errno($conn)) {
+    echo "DB Fail".mysqli_connect_error();
+  }
+  else {
+    $result = mysql_qurry($query);
+    
+    while ($field = mysql_fetch_field($result)) {
+      echo $field->name;
+      echo " ";
+    }
+  }
+}
+?>
