32e61be5180a7c8725b5672c64fc213c757bbe5b
moonwon <valentiney0214@gmail.com>
2014-07-12 00:16:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   if ($conn) {
     echo "DB Connected";
     
-    $result = mysql_query("SELECT * FROM user;", $conn);
+    $result = mysql_query("select * from user;", $conn);
     
     if ($result) {
       echo "<br>QUERY SUCCESS";
