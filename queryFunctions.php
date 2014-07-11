a53d886d3e6255f96165bd09c2d150d17a9fa3e2
moonwon <valentiney0214@gmail.com>
2014-07-11 23:02:42 +0900

    Create queryFunctions.php

---
+++ queryFunctions.php
@@ -0,0 +1,22 @@
+<?php
+
+fuction testQuery($db_name, $query) {
+
+  $db_host = "localhost";
+  $db_user = "root";
+
+  $conn = mysqli_connect($db_host, $db_user, "", $db_name);
+  if (mysqli_connect_errno($conn)) {
+    echo "DB Fail".mysqli_connect_error();
+  }
+  else {
+    $result = mysql_qurry($query);
+    
+    while ($field = mysql_fetch_field($result)) {
+      echo $field->name;
+      echo " ";
+    }
+  }
+}
+
+?>
